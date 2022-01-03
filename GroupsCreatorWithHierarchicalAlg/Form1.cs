4293b5b554f0bb2c2444fda32b138820ab48bc23
melih <melih.kacaman@stu.fsm.edu.tr>
2022-01-03 13:16:01 +0100

    Initial commit !

--- GroupsCreatorWithHierarchicalAlg/Form1.cs
+++ GroupsCreatorWithHierarchicalAlg/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace GroupsCreatorWithHierarchicalAlg
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Hello World!");
         }
     }
 }
